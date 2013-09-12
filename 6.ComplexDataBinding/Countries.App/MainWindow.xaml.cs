4ae5e1a057778e40416f87fc9cd0e8ed9fd8bad1
gparlakov <gparlakov@abv.bg>
2013-09-13 00:16:08 +0300

    6.ComplexDataBinding Started

---
+++ 6.ComplexDataBinding/Countries.App/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Countries.Data;
+
+namespace Countries.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new Data.CountriesViewModel();
+        }
+
+        private void Countries_SaveChangesButton(object sender, RoutedEventArgs e)
+        { 
+            using (var context = new Data.WorldEntities())
+            {
+               var x = e.Source;
+            }
+        }
+    }
+}
---
+++ 6.ComplexDataBinding/Countries.Data/CountriesViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countries.Data
+{
+    public class CountriesViewModel
+    {
+        public CountriesViewModel()
+        {
+            this.InitSources();
+        }
+
+        private void InitSources()
+        {
+            using (var context = new WorldEntities())
+            {
+                this.Countries = context.Countries.Include("Towns").ToList();
+                this.Towns = context.Towns.Include("Country").ToList();
+            }
+        }
+
+        public ICollection<Country> Countries { get; set; }
+
+        public ICollection<Town> Towns { get; set; }
+
+        public string GetLanguages(IEnumerable<Data.Language> languages)
+        {
+            if (languages.Count() == 0)
+            {
+                return "No languages selected";
+            }
+
+            var builder = new StringBuilder();
+
+            foreach (var lang in languages)
+            {
+                builder.Append(lang.Name + ", ");
+            }
+            builder.Length -= 2;
+
+            return builder.ToString();
+        }
+    }
+}
